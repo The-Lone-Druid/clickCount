8b16083f557abdc7bb61d8f18b94e28893604a54
The-Lone-Druid <zahid.786shaikh@gmail.com>
2021-07-31 12:07:49 +0530

    Production build

---
+++ dist/js/main.js
@@ -0,0 +1,84 @@
+// UI Variables
+const clickCountHolder = document.querySelector('#click-count');
+
+// Localstorage methods
+const commitToStorage = () => {
+
+    if(localStorage.getItem('clickcount')) {
+        let currentValue = JSON.parse(localStorage.getItem('clickcount'));
+        if(!(clickCountHolder.value >= 100000)) {
+            let addingValue = parseInt(currentValue) + 1;
+            localStorage.setItem('clickcount', JSON.stringify(addingValue));
+        }
+    } else {
+        let currentValue = 1;
+        localStorage.setItem('clickcount', JSON.stringify(currentValue));
+    }
+}
+
+const removeFromStorage = () => {
+    if(localStorage.getItem('clickcount')) {
+        let currentValue = JSON.parse(localStorage.getItem('clickcount'));
+        let addingValue = 0;
+        if(!(clickCountHolder.value <= 0)) {
+            addingValue = parseInt(currentValue) - 1;
+            localStorage.setItem('clickcount', JSON.stringify(addingValue));
+        }
+    }
+}
+
+function clearFromLocalStorage() {
+    localStorage.removeItem('clickcount');
+}
+
+// clearFromLocalStorage();
+
+// Methods
+
+const increaseCount = () => {
+    let value = clickCountHolder.value;
+    clickCountHolder.value = parseInt(value) + 1;
+    commitToStorage();
+};
+
+const decreaseCount = () => {
+    let value = clickCountHolder.value;
+    if(!(value <= 0)) {
+        clickCountHolder.value = parseInt(value) - 1;
+    }
+    removeFromStorage();
+};
+
+const clearCount = () => {
+    let value = 0;
+    clickCountHolder.value = parseInt(value);
+    clearFromLocalStorage();
+};
+
+// Events
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    if(localStorage.getItem('clickcount')) {
+        clickCountHolder.value = parseInt(JSON.parse(localStorage.getItem('clickcount')));
+    }
+})
+
+clickCountHolder.addEventListener('change', (e) => {
+    if(e.target.value < 0) {
+        e.target.value = 0;
+        Swal.fire(
+            'Negative value detected',
+            'Values less than 0 are not allowed!',
+            'error'
+        )
+    } else if(e.target.value > 100000) {
+        e.target.value = 0;
+        Swal.fire(
+            'Larger value detected',
+            'Currently larger value is set to 100K!',
+            'error'
+        )
+    } else {
+        commitToStorage();
+    }
+});
